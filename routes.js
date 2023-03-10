68ef0dbb61cfdb9253e3dc9f47c655362905e267
anmol <anmol@geekyants.com>
2023-03-10 17:13:13 +0530

    showed previous  reminders along with form

--- routes.js
+++ routes.js
@@ -2,14 +2,18 @@ const express = require("express");
 const dbCalls = require("./db/main");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.render("main", { layout: "index" });
+app.get("/", async (req, res) => {
+  let reminders = [];
+  try {
+    reminders = await dbCalls.getReminders();
+  } catch (e) {}
+  res.render("main", { layout: "index", reminders: reminders });
 });
 
 app.post("/save-reminder", async (req, res) => {
   try {
     const reminder = await dbCalls.saveReminder(req.body);
-    res.status(200).send(reminder);
+    res.status(200).render("thank-you", { layout: "index", data: reminder });
   } catch (e) {
     res.status(500).send(e);
   }
